1380876808be06e48029483355e893c816a76dd9
Kaphotics <kaphotics@gmail.com>
2016-05-19 19:22:30 -0700

    Initial Commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Austyn
+ * Date: 10/30/2015
+ * Time: 8:04 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace clickAuto
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label L_Position;
+		private System.Windows.Forms.Button B_Go;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.L_Position = new System.Windows.Forms.Label();
+            this.B_Go = new System.Windows.Forms.Button();
+            this.nud_x1 = new System.Windows.Forms.NumericUpDown();
+            this.nud_y1 = new System.Windows.Forms.NumericUpDown();
+            this.nud_y2 = new System.Windows.Forms.NumericUpDown();
+            this.nud_x2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nud_sleep = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_y1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_y2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_sleep)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // L_Position
+            // 
+            this.L_Position.Location = new System.Drawing.Point(12, 9);
+            this.L_Position.Name = "L_Position";
+            this.L_Position.Size = new System.Drawing.Size(100, 30);
+            this.L_Position.TabIndex = 3;
+            this.L_Position.Text = "X: {0}\r\nY: {1}";
+            // 
+            // B_Go
+            // 
+            this.B_Go.Location = new System.Drawing.Point(54, 9);
+            this.B_Go.Name = "B_Go";
+            this.B_Go.Size = new System.Drawing.Size(75, 30);
+            this.B_Go.TabIndex = 5;
+            this.B_Go.Text = "Run";
+            this.B_Go.UseVisualStyleBackColor = true;
+            this.B_Go.Click += new System.EventHandler(this.B_Go_Click);
+            // 
+            // nud_x1
+            // 
+            this.nud_x1.Location = new System.Drawing.Point(35, 45);
+            this.nud_x1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nud_x1.Name = "nud_x1";
+            this.nud_x1.Size = new System.Drawing.Size(57, 20);
+            this.nud_x1.TabIndex = 6;
+            this.nud_x1.Value = new decimal(new int[] {
+            270,
+            0,
+            0,
+            0});
+            // 
+            // nud_y1
+            // 
+            this.nud_y1.Location = new System.Drawing.Point(35, 64);
+            this.nud_y1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nud_y1.Name = "nud_y1";
+            this.nud_y1.Size = new System.Drawing.Size(57, 20);
+            this.nud_y1.TabIndex = 7;
+            this.nud_y1.Value = new decimal(new int[] {
+            270,
+            0,
+            0,
+            0});
+            // 
+            // nud_y2
+            // 
+            this.nud_y2.Location = new System.Drawing.Point(98, 64);
+            this.nud_y2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nud_y2.Name = "nud_y2";
+            this.nud_y2.Size = new System.Drawing.Size(57, 20);
+            this.nud_y2.TabIndex = 9;
+            this.nud_y2.Value = new decimal(new int[] {
+            800,
+            0,
+            0,
+            0});
+            // 
+            // nud_x2
+            // 
+            this.nud_x2.Location = new System.Drawing.Point(98, 45);
+            this.nud_x2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nud_x2.Name = "nud_x2";
+            this.nud_x2.Size = new System.Drawing.Size(57, 20);
+            this.nud_x2.TabIndex = 8;
+            this.nud_x2.Value = new decimal(new int[] {
+            1250,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 26);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X1\r\nY1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 26);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "X2\r\nY2";
+            // 
+            // nud_sleep
+            // 
+            this.nud_sleep.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_sleep.Location = new System.Drawing.Point(135, 19);
+            this.nud_sleep.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nud_sleep.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_sleep.Name = "nud_sleep";
+            this.nud_sleep.Size = new System.Drawing.Size(57, 20);
+            this.nud_sleep.TabIndex = 12;
+            this.nud_sleep.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "sleep (ms)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 96);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nud_sleep);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud_y2);
+            this.Controls.Add(this.nud_x2);
+            this.Controls.Add(this.nud_y1);
+            this.Controls.Add(this.nud_x1);
+            this.Controls.Add(this.B_Go);
+            this.Controls.Add(this.L_Position);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AUTOCLICK";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.nud_x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_y1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_y2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_sleep)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+        private System.Windows.Forms.NumericUpDown nud_x1;
+        private System.Windows.Forms.NumericUpDown nud_y1;
+        private System.Windows.Forms.NumericUpDown nud_y2;
+        private System.Windows.Forms.NumericUpDown nud_x2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nud_sleep;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
+
+namespace clickAuto
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+        
+        private const int MOUSEEVENTF_MOVE = 0x0001; /* mouse move */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
+        private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010; /* right button up */
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020; /* middle button down */
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040; /* middle button up */
+        private const int MOUSEEVENTF_XDOWN = 0x0080; /* x button down */
+        private const int MOUSEEVENTF_XUP = 0x0100; /* x button down */
+        private const int MOUSEEVENTF_WHEEL = 0x0800; /* wheel button rolled */
+        private const int MOUSEEVENTF_VIRTUALDESK = 0x4000; /* map to entire virtual desktop */
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000; /* absolute move */
+        
+		public MainForm()
+		{
+			InitializeComponent();
+			currentCursorPosition.Tick += getCursorPosition;
+			currentCursorPosition.Enabled = true;
+		}
+		private void getCursorPosition(object sender, EventArgs e)
+	    {
+			L_Position.Text = String.Format("X: {0}{2}Y: {1}", Cursor.Position.X, Cursor.Position.Y, Environment.NewLine);
+        }
+        // Update current cursor position
+        private readonly Timer currentCursorPosition = new Timer
+        {
+            Interval = 50,
+            Enabled = false,
+        };
+
+
+        private static void performLeftClick(int x, int y)
+		{
+	        Cursor.Position = new System.Drawing.Point(x, y);
+	        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); // left button down
+	        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); // left button up
+		}		
+		private static void performRightClick(int x, int y)
+		{
+	        Cursor.Position = new System.Drawing.Point(x, y);
+	        mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0); // right button down
+	        mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0); // right button up
+		}
+
+        private bool running;
+        private void B_Go_Click(object sender, EventArgs e)
+		{
+		    if (running)
+		        return;
+            Random rnd = new Random();
+		    int x1 = (int)nud_x1.Value;
+            int x2 = (int)nud_x2.Value;
+            int y1 = (int)nud_y1.Value;
+            int y2 = (int)nud_y2.Value;
+		    int sleepms = (int)nud_sleep.Value;
+            if (sleepms <= 0)
+                sleepms = 1;
+
+            new Thread(() =>
+            {
+                running = true;
+                while (ModifierKeys != Keys.Control)
+                {
+                    performLeftClick(rnd.Next(x1,x2), rnd.Next(y1,y2));
+                    Thread.Sleep(sleepms);
+                }
+                running = false;
+            }).Start();
+		}
+	}
+}
